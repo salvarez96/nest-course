ec0c1b7c0ffdf003a8cc11265dde37fb38ba7165
sacimperialeads <santiago@imperialeads.com>
2025-03-13 17:44:32 -0500

    Create config file to handle all env variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CONFIG } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(CONFIG.PORT);
 }
-bootstrap();
+bootstrap()
+  .then(() => console.log('Project is running'))
+  .catch((error) =>
+    console.error("There's a problem with the project: ", error),
+  );
