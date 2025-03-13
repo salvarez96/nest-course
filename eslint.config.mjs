984cceee51e3e1df9edd0d7579a783d2f7c9498a
sacimperialeads <santiago@imperialeads.com>
2025-03-13 17:59:27 -0500

    Configure linter and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,7 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
+      ecmaVersion: 9,
       sourceType: 'module',
       parserOptions: {
         projectService: true,
