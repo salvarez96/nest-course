f1e37de2ce8f869650eff60a94342427470baec5
sacimperialeads <santiago@imperialeads.com>
2025-03-14 16:15:23 -0500

    Learn how to use route params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+type DoubleParamEndpoint = {
+  id: string;
+  name: string;
+};
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
@@ -13,5 +17,17 @@ export class AppController {
   @Get('new-endpoint')
   getNewEndpoint(): string {
     return 'This is the new endpoint';
+  }
+
+  @Get('new-endpoint/:id')
+  getNewEndpointWithId(@Param('id') id: string): string {
+    return 'This is the new endpoint with id: ' + id;
+  }
+
+  @Get('new-endpoint/:id/route/:name')
+  getNewEndpointWithIdAndName(
+    @Param() { id, name }: DoubleParamEndpoint,
+  ): string {
+    return 'This is the new endpoint with id: ' + id + ' and name: ' + name;
   }
 }
