9b6d46c24ac1e71f7029d90f1e24443d67c84d53
sacimperialeads <santiago@imperialeads.com>
2025-03-14 12:50:53 -0500

    🚨 Modify prettier settings

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ import { CONFIG } from './config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(CONFIG.PORT);
+  console.log(`App is running on port ${CONFIG.PORT}`);
 }
-bootstrap()
-  .then(() => console.log('Project is running'))
-  .catch((error) =>
-    console.error("There's a problem with the project: ", error),
-  );
+void bootstrap();
