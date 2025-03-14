d8756a5dac9048a69a09ab03273f23ec23338efd
sacimperialeads <santiago@imperialeads.com>
2025-03-14 12:56:45 -0500

    ⚗️ Learn about decorators with first GET method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('new-endpoint')
+  getNewEndpoint(): string {
+    return 'This is the new endpoint';
+  }
 }
